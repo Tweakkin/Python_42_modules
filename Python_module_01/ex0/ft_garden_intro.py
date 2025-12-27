1ac969663119130b48214f0d777e4f8d2cf953f9
Tweakkin <yahyaboukhmira7@gmail.com>
2025-12-28 00:47:23 +0100

    Done

---
+++ Python_module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+A simple script to display information about a garden plant.
+This is the first entry point for the garden management system.
+"""
+if __name__ == "__main__":
+	name = "Rose"
+	height = "25cm"
+	age = "30 days"
+	print("=== Welcome to My Garden ===")
+	print("Plant:", name)
+	print("Height:", height)
+	print("Age:", age)
+	print("\n", end="")
+	print("=== End of Program ===")
