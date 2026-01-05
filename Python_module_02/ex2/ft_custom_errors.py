5879659a78365ec5f8cb766961827508b3d577ee
Tweakkin <yahyaboukhmira7@gmail.com>
2026-01-06 00:39:30 +0100

    done

--- Python_module_02/ex2/ft_custom_errors.py
+++ Python_module_02/ex2/ft_custom_errors.py
@@ -1,16 +1,32 @@
 class GardenError(Exception):
+    """
+    Base class for all garden-related errors.
+    Inherits from the built-in Exception class so it behaves like a real error.
+    """
     pass
 
 class PlantError(GardenError):
+    """
+    Specific error for plant-related issues.
+    """
     pass
 
 class WaterError(GardenError):
+    """
+    Specific error for water-related issues .
+    """
     pass
 
 def test_plant_error():
-    raise PlantError("the tomato plant is wilting!")
+    """
+    Simulates a plant problem by raising a PlantError.
+    """
+    raise PlantError("The tomato plant is wilting!")
 
 def test_water_error():
+    """
+    Simulates a water problem by raising a WaterError.
+    """
     raise WaterError("Not enough water in the tank!")
 
 print("=== Custom Garden Errors Demo ===")
@@ -19,6 +35,7 @@ print("\nTesting PlantError...")
 try:
     test_plant_error()
 except PlantError as e:
+    # Standard exception handling: Catching the exact error type.
     print(f"Caught PlantError: {e}")
 
 print("\nTesting WaterError...")
@@ -31,8 +48,12 @@ print("\nTesting catching all garden errors...")
 try:
     test_plant_error()
 except GardenError as e:
+# I used subtype polymorphism where specific child classes
+# like PlantError are treated as their general parent class, GardenError.
     print(f"Caught a garden error: {e}")
 try:
     test_water_error()
 except GardenError as e:
     print(f"Caught a garden error: {e}")
+
+print("\nAll custom error types work correctly!")
