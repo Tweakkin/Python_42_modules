f21227e3fd83e912c5f25d6ce94f14c1f01aba32
Tweakkin <yahyaboukhmira7@gmail.com>
2026-02-04 19:06:36 +0100

    needs flake8 fixing

---
+++ Python_module_04/ex3/ft_vault_security.py
@@ -0,0 +1,36 @@
+import sys
+
+def  main() -> None:
+	print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
+
+	print("Initiating secure vault access...")
+	try:
+		with open("../data_samples/classified_data.txt", 'r')  as file:
+			print("Vault connection established with failsafe protocols\n")
+			print("SECURE EXTRACTION:")
+			content = file.read()
+			print(content)
+	except FileNotFoundError:
+		print("ERROR: Storage vault not found")
+		sys.exit(1)
+	except PermissionError:
+		print("ERROR: Storage vault access denied.")
+		sys.exit(1)
+	
+	try:
+		print("\nSECURE PRESERVATION:")
+		with open("../data_samples/classified_data.txt", 'a')  as file:
+			file.write("\n[CLASSIFIED] New security protocols archived")
+			print("[CLASSIFIED] New security protocols archived")
+		if file.closed:
+			print("Vault automatically sealed upon completion")
+	except FileNotFoundError:
+		print("ERROR: Storage vault not found")
+		sys.exit(1)
+	except PermissionError:
+		print("ERROR: Storage vault access denied.")
+		sys.exit(1)
+
+	print("\nAll vault operations completed with maximum security.")
+
+main()
