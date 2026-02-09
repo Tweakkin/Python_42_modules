9ea2f3c89c67e0cbb95ff589f4011c1d67b5dc7b
Tweakkin <yahyaboukhmira7@gmail.com>
2026-02-09 02:21:30 +0100

    Done with NumericProcessor

--- Python_module_05/ex0/stream_processor.py
+++ Python_module_05/ex0/stream_processor.py
@@ -13,14 +13,38 @@ class DataProcessor(ABC):
     
     #Format the output string
     def format_output(self, result: str) -> str:
-        return f"{result} yahya"
+        return f"Output: {str}"
 
 class NumericProcessor(DataProcessor):
     def process(self, data: any) -> str:
-        return ("Hello world!")
+        try:
+            if not self.validate(data):
+                return "Error: Data failed validation from NumericProcessor"
+            
+            if isinstance(data, (int, float)):
+                data = [data]
+
+            total = sum(data)
+            len_data = len(data)
+            if len_data == 0:
+                average = 0.0
+            else:
+                average = total / len_data
+            return f"Processed {len_data} numeric values, sum={total}, avg={average:.1f}"
+        except Exception as e:
+            return f"Error processing data: {str(e)}"
     
     def validate(self, data: any) -> bool:
-        return True
+        if isinstance(data, (int, float)):
+            return True
+        
+        if isinstance(data, list):
+            for item in data:
+                if not isinstance(item, (int, float)):
+                    return False
+            return True
+        else:
+            return False
 
 class TextProcessor(DataProcessor):
     def process(self, data: any) -> str:
@@ -37,5 +61,15 @@ class LogProcessor(DataProcessor):
     def validate(self, data: any) -> bool:
         return True
 
-num = NumericProcessor()
-print(f"{num.format_output("hello")}")
+def main() -> None:
+    print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
+
+    print("Initializing Numeric Processor...")
+    num_p = NumericProcessor()
+    data = [1, 2, 3, 4, 5]
+    print(f"Processing data: {data}")
+    print(f"Validation {"Numeric data verified" if num_p.validate(data) == True else "Failed"}")
+    print(f"Output: {num_p.process(data)}")
+
+
+main()
