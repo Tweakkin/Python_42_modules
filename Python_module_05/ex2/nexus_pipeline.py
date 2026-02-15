f92c4f30e760394252c92ce29ba1f6ae850e5bd5
Tweakkin <yahyaboukhmira7@gmail.com>
2026-02-15 01:22:25 +0100

    Done with the three first stages, still a lot to work on

--- Python_module_05/ex2/nexus_pipeline.py
+++ Python_module_05/ex2/nexus_pipeline.py
@@ -1,6 +1,32 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Dict, Union, Optional, Protocol
 
+class ProcessingStage(Protocol):
+	def process(self, data: Any) -> Any:
+		pass
+
+class InputStage:
+	def process(self, data: Any) -> Dict:
+		if data is None:
+			raise ValueError("InputStage: Data cannot be None")
+		if isinstance(data, dict):
+			return data
+		return {"data": data}
+		
+
+class TransformStage:
+	def process(self, data: Dict) -> Dict:
+		if not isinstance(data, dict):
+			raise ValueError("TransformStage: Data must be dict.")
+		data.update({"processed": True})
+		return data
+
+class OutputStage:
+	def process(self, data: dict) -> str:
+		if not isinstance(data, dict):
+			raise ValueError("OutputStage: Data must be dict.")
+		return f"Final Result: {data}"
+
 class ProcessingPipeline(ABC):
 	pass
 
@@ -22,6 +48,10 @@ def main() -> None:
 
 if __name__ == "__main__":
 	main()
-	yahya = builder()
-	testing(yahya)
+	yahya = InputStage()
+	data = yahya.process("Hello world!")
+	yahya_t = TransformStage()
+	data = yahya_t.process(data)
+	yahya_o = OutputStage()
+	print(yahya_o.process(data))
 
